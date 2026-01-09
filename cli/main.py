82714a589047b788ed5f933bc90a5d5a7ca40853
Grant Herman <gherma@meta.com>
2026-01-08 19:30:49 -0500

    ls basic

--- cli/main.py
+++ cli/main.py
@@ -1,18 +1,34 @@
 import sys, termios
 import os
 
+
+VIRTUAL_DIR = os.getcwd()
+DEBUGGING=False
+
+def __DEBUG__(message):
+    if DEBUGGING:
+        sys.stdout.write(message)
+
+
 def CommandPWD():
     sys.stdout.write(os.getcwd())
-    
+
+
+
+def _walk_directory(path):
+    print("DIRRR", path)
+    for item in os.listdir(path):
+        tag = "file" if os.path.isfile(item) else "dir"
+        sys.stdout.write(f"------ {tag} {item}\n")
+
 def CommandLS(path):
     if len(path) <= 0:
-        dir = os.listdir(os.getcwd())
-        for item in dir:
-            tag = "file" if os.path.isfile(item) else "dir"
-            sys.stdout.write(f"------ {tag} {item}")
+        _walk_directory(VIRTUAL_DIR)
+        return
+    _walk_directory(path)
+       
 
-
-def main():
+def main(args):
    sys.stdout.write(f"WGAF v1.0 on {os.name}\n")
    sys.stdout.write('Type "help", "copyright", "credits" or "license" for more information.\n')
    sys.stdout.write('$ ')
@@ -20,7 +36,9 @@ def main():
     command = input()
     parse_command = command.split(" ")
     root_command = parse_command[0]
-    arguments = parse_command[1]
+    arguments = ""
+    if len(parse_command) > 1:
+        arguments = parse_command[1]
     match root_command:
         case "exit":
             break
@@ -28,6 +46,7 @@ def main():
             CommandPWD()
             break
         case "ls":
+                print("ARGUEMNTS", len(arguments))
                 CommandLS(arguments)
                 break
 
@@ -38,4 +57,4 @@ def main():
 		
 
 if __name__ == "__main__":
-	main()
+    main(sys.argv[:1])
